f22887bbe4d6261343bd9a676140d949575cfa13
annazeit <77589107+annazeit@users.noreply.github.com>
2021-08-31 00:40:03 +0100

    StockPriceOptimization and QuickSort.

---
+++ QuickSort.fs
@@ -0,0 +1,49 @@
+﻿namespace ElementsOfProgrammingInterview
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+module QuickSort =
+    
+
+    // 1; 3; 5; 2; 4; 6 -> 1; 2; 3; 4; 5; 6
+    // 1; 2; 3; 4; 5; 6
+    let merge (array: int array) startIndex endIndex median =
+        let mutable i, j = startIndex, (median + 1)
+        while j <= endIndex do
+            if array.[i] > array.[j] then
+                let t = array.[i]
+                array.[i] <- array.[j]
+                array.[j] <- t
+
+            i <- i + 1
+
+            if i = j then
+                j <- j + 1
+
+
+    let rec quicSort (array: int array) startIndex endIndex =
+        if startIndex >= endIndex then ()
+        else
+            let median = startIndex + (endIndex - startIndex) / 2
+            quicSort array startIndex median
+            quicSort array (median + 1) endIndex
+            merge array startIndex endIndex median
+
+
+
+[<TestClass>]
+type TestQuickSort () =
+
+    [<TestMethod>]
+    member this.Test () =
+        let testCases = [
+            [1; 3; 5; 2; 4; 6 ], [1; 2; 3; 4; 5; 6]
+            [ 2; 3; 1], [1; 2; 3]
+            [ 5; 10; 3; 4; 7; 14 ], [ 3; 4; 5; 7; 10; 14]
+        ]
+        for data, expectedResult in testCases do
+            let source = List.toArray data
+            QuickSort.quicSort source 0 (source.Length - 1)
+            Assert.IsTrue(System.Linq.Enumerable.SequenceEqual(source, expectedResult))
+       
---
+++ StockPriceOptimization.fs
@@ -0,0 +1,56 @@
+﻿// Devide and conquire method to find optimized stock buy-sell price.
+
+namespace ElementsOfProgrammingInterview
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+module StockPriceOptimization =
+
+    let min (array: int array) startIndex endIndex =
+        let mutable min = array.[startIndex]
+        for i = (startIndex + 1) to endIndex do
+            let item = array.[i]
+            if item < min then min <- item
+        min
+
+    let max (array: int array) startIndex endIndex =
+        let mutable max = array.[startIndex]
+        for i = (startIndex + 1) to endIndex do
+            let item = array.[i]
+            if item > max then max <- item
+        max
+
+    let rec maximizeProfit array startIndex endIndex =
+        let indexDif = endIndex - startIndex
+        if indexDif < 1 then 0
+        else
+            let leftEndIndex = startIndex + indexDif / 2
+            let profitMaximizedParts =
+                let profitMaximizedLeft = maximizeProfit array startIndex leftEndIndex
+                let prifitMaximizedRight = maximizeProfit array (leftEndIndex + 1) endIndex
+                Math.Max(profitMaximizedLeft, prifitMaximizedRight)
+            let profitMaximizedFull =
+                let leftMin = min array startIndex leftEndIndex
+                let rightMax = max array (leftEndIndex + 1) endIndex
+                if leftMin < rightMax then rightMax - leftMin
+                else 0
+            Math.Max(profitMaximizedParts, profitMaximizedFull)
+
+
+[<TestClass>]
+type TestStockPriceOptimization () =
+
+    [<TestMethod>]
+    member this.Test () =
+        let testCases = [
+            [| 1; 5; 3; 2 |], 4
+            [| 7; 2; 8; 9 |], 7
+            [| 1; 6; 7; 9 |], 8
+            [| 9; 7; 4; 1 |], 0
+            [| 10; 5; 1; 0 |], 0
+        ]
+        for data, expectedProfitMaximized in testCases do
+            let profitMaximized = StockPriceOptimization.maximizeProfit data 0 (data.Length - 1)
+            Assert.IsTrue((expectedProfitMaximized = profitMaximized))
+       
